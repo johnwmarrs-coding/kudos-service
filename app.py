2452e2ec1479a2e0162b75c8880e645858f2deec
John Marrs <johnwmarrs.coding@gmail.com>
2024-04-11 11:17:14 -0400

    Added db config and models

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import flask
+from flask import Flask
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+if os.environ.get('ENV') == 'production':
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@hostname/database_name'
+else:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Kudos(db.Model):
+    path = db.Column(db.String(80), primary_key=True)
+    kudos = db.Column(db.Integer, nullable=False, default=0)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(255), nullable=True)
+    
 
 @app.route("/")
 def index():
     return 'The kudos-service is running!'
 
-@app.route("/kudos/overview", methods=["GET"])
+@app.route("/kudos", methods=["GET"])
 def overview():
     return "Overview of kudos objects"
 
